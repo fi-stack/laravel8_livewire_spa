7cf22a121981d05492e6fcb74eebc188bed5ef90
rafi-windows <rafi_n_o@github.com>
2020-12-05 06:49:03 +0700

    Membuat Component Livewire

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', \App\Http\Livewire\Post\Index::class)->name('post.index');
+Route::get('/create', \App\Http\Livewire\Post\Create::class)->name('post.create');
+Route::get('/edit/{id}', \App\Http\Livewire\Post\Edit::class)->name('post.edit');
