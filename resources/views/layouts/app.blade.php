1b7c05dbf9b4ccd0974d86f33b7a13831d3d41d3
rafi-windows <rafi_n_o@github.com>
2020-12-05 06:28:15 +0700

    Install Laravel Livewire

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel=“stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <livewire:styles />
+    <livewire:scripts />
+</head>
+
+<body>
+    <div class="container mt-5 mb-5">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
