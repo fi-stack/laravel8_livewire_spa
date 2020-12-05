e16a40eec2dbd978ad893f0cd4fc98938f4ad199
rafi-windows <rafi_n_o@github.com>
2020-12-05 08:57:13 +0700

    Edit & Update Data ke Database

--- app/Http/Livewire/Post/Edit.php
+++ app/Http/Livewire/Post/Edit.php
@@ -2,10 +2,59 @@
 
 namespace App\Http\Livewire\Post;
 
+use App\Models\Post;
 use Livewire\Component;
 
 class Edit extends Component
 {
+    /**
+     * define public variable
+     */
+    public $postId;
+    public $title;
+    public $content;
+
+    /**
+     * mount or construct function
+     */
+    public function mount($id)
+    {
+        $post = Post::find($id);
+
+        if ($post) {
+            $this->postId = $post->id;
+            $this->title = $post->title;
+            $this->content = $post->content;
+        }
+    }
+
+    /**
+     * update function
+     */
+    public function update()
+    {
+        $this->validate([
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        if ($this->postId) {
+            $post = Post::find($this->postId);
+
+            if ($post) {
+                $post->update([
+                    'title' => $this->title,
+                    'content' => $this->content
+                ]);
+            }
+        }
+        // flash message
+        session()->flash('message', 'Data berhasil diupdate.');
+
+        // redirect
+        return redirect()->route('post.index');
+    }
+
     public function render()
     {
         return view('livewire.post.edit');
--- resources/views/livewire/post/edit.blade.php
+++ resources/views/livewire/post/edit.blade.php
@@ -1,3 +1,28 @@
 <div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+    <div class="card">
+        <div class="card-body">
+            <form wire:submit.prevent="update">
+                <input type="hidden" wire:model="postId">
+                <div class="form-group">
+                    <label>Title</label>
+                    <input type="text" wire:model="title" class="form-control @error('title') is-invalid @enderror" placeholder="Masukkan Title">
+                    @error('title')
+                    <span class="invalid-feedback">
+                        {{ $message }}
+                    </span>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label>Konten</label>
+                    <textarea wire:model="content" class="form-control @error('content') is-invalid @enderror" rows="4" placeholder="Masukkan Konten"></textarea>
+                    @error('content')
+                    <span class="invalid-feedback">
+                        {{ $message }}
+                    </span>
+                    @enderror
+                </div>
+                <button type="submit" class="btn btn-primary">Update</button>
+            </form>
+        </div>
+    </div>
 </div>
