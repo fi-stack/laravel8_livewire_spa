75ea090f6f6b77d9950b353ae2f8a4e2d32d9ac0
rafi-windows <rafi_n_o@github.com>
2020-12-05 08:21:14 +0700

    Insert Data ke Database

--- app/Http/Livewire/Post/Create.php
+++ app/Http/Livewire/Post/Create.php
@@ -2,10 +2,38 @@
 
 namespace App\Http\Livewire\Post;
 
+use App\Models\Post;
 use Livewire\Component;
 
 class Create extends Component
 {
+    /**
+     * define public variabel
+     */
+    public $title;
+    public $content;
+
+    /**
+     * store function
+     */
+    public function store()
+    {
+        $this->validate([
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        $post = Post::create([
+            'title' => $this->title,
+            'content' => $this->content
+        ]);
+
+        // flash message
+        session()->flash('message', 'Data Berhasil Disimpan.');
+
+        // redirect
+        return redirect()->route('post.index');
+    }
     public function render()
     {
         return view('livewire.post.create');
--- resources/views/livewire/post/create.blade.php
+++ resources/views/livewire/post/create.blade.php
@@ -1,3 +1,27 @@
 <div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+    <div class="card">
+        <div class="card-body">
+            <form wire:submit.prevent="store">
+                <div class="form-group">
+                    <label>Title</label>
+                    <input type="text" wire:model="title" class="form-control @error('title') is-invalid @enderror" placeholder="Masukkan title">
+                    @error('title')
+                    <span class="invalid-feedback">
+                        {{ $message }}
+                    </span>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <label>Konten</label>
+                    <textarea wire:model="content" class="form-control @error('content') is-invalid @enderror" rows="4" placeholder="Masukkan Konten"></textarea>
+                    @error('content')
+                    <span class="invalid-feedback">
+                        {{ $message }}
+                    </span>
+                    @enderror
+                </div>
+                <button type="submit" class="btn btn-primary">Simpan</button>
+            </form>
+        </div>
+    </div>
 </div>
